8ecd73886c62d8909231099f9b49eaca98db2949
Nik <mushroomicus@gmail.com>
2022-08-19 17:34:59 +0300

    add project

---
+++ KKOApp/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  KKOApp
+//
+//  Created by VironIT on 19.08.22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
