da6a2a5aed95d0dc963426bb3f898ae1c63fe8d4
Nik_Gribok <mushroomicus@gmail.com>
2022-08-24 07:22:54 +0300

    redraw buying screen

--- KKOApp/Modules/TabBar Screens/BuyCoffee/BuyCoffeeVC.swift
+++ KKOApp/Modules/TabBar Screens/BuyCoffee/BuyCoffeeVC.swift
@@ -20,9 +20,9 @@ class BuyCoffeeVC: UIViewController, CoffeeProtocol {
     @IBOutlet weak var titleLabel: UILabel!
     @IBOutlet weak var descriptionLabel: UILabel!
     @IBOutlet weak var priceLabel: UILabel!
-    @IBOutlet weak var oatMilkButton: UIButton!
-    @IBOutlet weak var soyMilkButton: UIButton!
-    @IBOutlet weak var almondMilkButton: UIButton!
+    @IBOutlet weak var sizeSButton: UIButton!
+    @IBOutlet weak var sizeMButton: UIButton!
+    @IBOutlet weak var sizeLButton: UIButton!
     @IBOutlet weak var cardBackgroundView: UIView!
     
     var name: String = ""
@@ -34,17 +34,17 @@ class BuyCoffeeVC: UIViewController, CoffeeProtocol {
     override func viewDidLoad() {
         super.viewDidLoad()
         blureFone()
+        blureView()
         configure(title: name, descript: descript, price: price, image: imageName)
     }
     
     private func configure(title: String, descript: String, price: UInt, image: String) {
         titleLabel.text = title
         descriptionLabel.text = descript
-        priceLabel.text = "\(price)$"
+        priceLabel.text = "\(price).0"
         coffeeImageView.image = UIImage().resizeImage(image: .init(named: image)!, targetSize: .init(width: 400, height: 400))
         foneImageView.image = UIImage().resizeImage(image: .init(named: image)!, targetSize: .init(width: 400, height: 400))
         coffeeImageView.layer.cornerRadius = 40
-        cardBackgroundView.layer.cornerRadius = 40
     }
     
     private func blureFone() {
@@ -52,5 +52,13 @@ class BuyCoffeeVC: UIViewController, CoffeeProtocol {
         let effect = UIVisualEffectView(effect: blure)
         effect.frame = foneImageView.bounds
         foneImageView.addSubview(effect)
+    }
+    
+    private func blureView() {
+        let blure = UIBlurEffect(style: .dark)
+        let effect = UIVisualEffectView(effect: blure)
+        effect.frame = cardBackgroundView.bounds
+        cardBackgroundView.addSubview(effect)
+        cardBackgroundView.layer.cornerRadius = 40
     }
 }
